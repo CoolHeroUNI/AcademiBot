4cc1a1887d3931a85dfaa72834781d9e4f069e34
Marco Antonio <mvelar@uni.pe>
2019-07-21 03:40:30 -0500

    Error en require
    Se corrige error en path al realizar require

--- routes/clasificacion.js
+++ routes/clasificacion.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
-const middleware = require('../public/javascripts/middleware');
+const middleware = require('../src/middleware');
 
 router.get('/', middleware.activeSession, (req, res) => {
   request(process.env.URL + "webhook/muestra/facultades/"+process.env.PROCESS_KEY, (err, response, body) => {
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const middleware = require('../public/javascripts/middleware');
+const middleware = require('../src/middleware');
 
 router.get('/', middleware.unactiveSession, (req, res) => {
   res.render('login', {});
