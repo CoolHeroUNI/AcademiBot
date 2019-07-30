fa679a486996e163911d7a9a8253c2dc74447ae3
Marco Antonio <mvelar@uni.pe>
2019-07-29 23:12:49 -0500

    Develop Busqueda de mejores maneras de reflejar la url principal del servidor sin tener que recurrir a una variable de entorno

--- routes/inicio.js
+++ routes/inicio.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const inicio = (req,res) => {
+  console.log(req.get('host'), req.hostname);
   res.send("Bienvenido a AcademiBot.");
 };
 router.get('/', inicio);
