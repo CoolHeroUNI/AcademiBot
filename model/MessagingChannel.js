f446d6451f98dae27249e68e68d6027f094488cd
Marco <mvelar@uni.pe>
2019-10-14 12:43:40 -0500

    MessagingChannel La interfaz MessagingChannel definira los metodos de las clases que busquen interactuar con los usuarios del ChatBot

--- model/MessagingChannel.js
+++ model/MessagingChannel.js
@@ -1,3 +1,9 @@
+/**
+ * @class MessagingChannel
+ * Interfaz para el desarrollo de los canales de mensajeria para un ChatBot, debe contar con metodos para comenzar la
+ * interaccion con un usuario, enviar mensajes, URLs, texto mixto con URLs, adjuntos y adjuntos de forma secuencial,
+ * obtener la informacion de un usuario, enviar botones de respuesta rapida y un menu de opciones
+ */
 class MessagingChannel {
     constructor() {
         const methods = [
@@ -9,7 +15,8 @@ class MessagingChannel {
             "sendSecuentialAttachments",
             "getUserInfo",
             "sendReplyButtons",
-            "sendOptionsMenu"
+            "sendOptionsMenu",
+            "endInteraction"
         ];
         for (const method of methods) {
             if (!this[method]) {
@@ -19,4 +26,89 @@ class MessagingChannel {
     }
 }
 
+/**
+ * @method startInteraction
+ * Inicia la interaccion con un usuario y determina si esta fue exitosa o no
+ * @param {Number} userId
+ * @returns {Promise<Boolean>}
+ */
+MessagingChannel.prototype.startInteraction = function(userId) {};
+
+/**
+ * @method sendText
+ * Envia un mensaje de texto a un usuario
+ * @param {Number} userId
+ * @param {String} text
+ * @returns {Promise}
+ */
+MessagingChannel.prototype.sendText = function(userId, text) {};
+
+/**
+ * @method sendURL
+ * Envia una URL a un usuario, dependiendo de las caracteristicas del canal de mensajeria puede generarse una vista
+ * previa
+ * @param {Number} userId
+ * @param {String} URL
+ * @returns {Promise}
+ */
+MessagingChannel.prototype.sendURL = function(userId, URL) {};
+
+/**
+ * @method sendTextWithURLs
+ * Envia un texto que puede contener URLs, que seran enviadas de forma secuencial luego de enviar el texto
+ * @param {Number} userId
+ * @param {String} text
+ * @returns {Promise}
+ */
+MessagingChannel.prototype.sendTextWithURLs = function(userId, text) {};
+
+/**
+ * @method sendAttachment
+ * Envia un archivo adjunto a un usuario, los parametros del envio dependen del canal de mensajeria, devuelve una cadena
+ * detallando el resultado de la operacion
+ * @param {Number} userId
+ * @param {Object} parameters
+ * @returns {Promise<String>}
+ */
+MessagingChannel.prototype.sendAttachment = function(userId, parameters) {};
+
+/**
+ * @method sendSecuentialAttachments
+ * Envia varios archivos adjuntos a un usuario de forma secuencial, devuelde una lista ordenada detallando el resultado
+ * de cada operacion
+ * @param {Number} userId
+ * @param {Object[]} parameterList
+ * @returns {Promise<String[]>}
+ */
+MessagingChannel.prototype.sendSecuentialAttachments = function(userId, parameterList) {};
+
+/**
+ * @method getUserInfo
+ * Devuelve la informacion del usuario almacenada en dicho canal de mensajeria
+ * @param {Number} userId
+ * @returns {Promise<Object>}
+ */
+MessagingChannel.prototype.getUserInfo = function(userId) {};
+
+/**
+ * @method sendReplyButtons
+ * Envia botones de un solo uso con respuestras predefinidas, los parametros dependen del canal de mensajeria
+ * @param userId
+ * @param {Object[]} parameters
+ */
+MessagingChannel.prototype.sendReplyButtons = function(userId, parameters) {};
+/**
+ * @method sendOptionsMenu
+ * Envia menu persistente con opciones predefinidas, los parametros dependen del canal de mensajeria
+ * @param {Number} userId
+ * @param {Object[]} parameters
+ */
+MessagingChannel.prototype.sendOptionsMenu = function(userId, parameters) {};
+/**
+ * @method endInteraction
+ * Culmina la interaccion con el usuario
+ * @param {Number} userId
+ * @returns {Promise}
+ */
+MessagingChannel.prototype.endInteraction = function(userId) {};
 module.exports = MessagingChannel;
