4306dd52300ceea6b0c66fa1191ebd2d950170fe
Marco <mvelar@uni.pe>
2019-10-23 12:35:28 -0500

    Try

--- src/Instances.js
+++ src/Instances.js
@@ -26,8 +26,9 @@ const Dialogflow = new dialogflow(projectId, language);
 const host = process.env.MYSQL_HOST;
 const user = process.env.MYSQL_USER;
 const pass = process.env.MYSQL_PASS;
+const db = process.env.MYSQ_DB;
 const port = process.env.MYSQL_PORT;
-const MySQL = new MySQLDataBase(host, user, pass, port);
+const MySQL = new MySQLDataBase(host, user, pass, db, port);
 
 const AcademiBot = new Bot();
 AcademiBot.setDataBase(MySQL);
