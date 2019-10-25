5112f313155f78f2867752ad843dfd11d8987e78
Marco <mvelar@uni.pe>
2019-10-25 10:20:28 -0500

    SQL

--- model/Classes/Bot.js
+++ model/Classes/Bot.js
@@ -128,6 +128,7 @@ Bot.prototype.detectCourses = function (user, message) {
  * @param {Curso[]} courses
  */
 Bot.prototype.sendCourses = function (user, courses) {
+    console.log(courses);
     const options = courses.map(course => {
         const option = {};
         const data = course.getData();
