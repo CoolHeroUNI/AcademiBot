033ca1d853aed855cf2fbf645e2ac44027951e1f
Marco <mvelar@uni.pe>
2019-10-25 14:10:51 -0500

    Final Part

--- src/AcademiBot.js
+++ src/AcademiBot.js
@@ -5,7 +5,7 @@ const amazondata = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: "us-east-1",
-    nombre: process.env.S3_BUCKET_NAME
+    nombre: process.env.AWS_S3_BUCKET_NAME
 };
 const AcademiBot = new Bot();
 AcademiBot.carga()
