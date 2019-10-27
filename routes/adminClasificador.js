228a570fefdb3932f43abcea481acad64a5f5db4
Marco <mvelar@uni.pe>
2019-10-27 17:32:27 -0500

    Clasificador v2.0

--- routes/adminClasificador.js
+++ routes/adminClasificador.js
@@ -4,10 +4,6 @@ const {S3, MySQL} = require('../src/Instances');
 const CacheHandler = require('../model/Classes/CacheHandler');
 const cache = new CacheHandler(1800);
 const submissionsFolder = process.env.ACADEMIBOT_SUBMISSIONS_FOLDER;
-const AcademiBot = require('../src/AcademiBot');
-
-
-
 
 router.get('/', (req, res) => {
   let Keys;
@@ -52,40 +48,6 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-
-  const tipe = req.query.tipo || 'image';
-  const page = req.query.page || 0;
-  const action = req.query.action;
-  console.log(req.body);
-  if (!action || !req.body.ruta) {
-    return res.send(404);
-  }
-  if (action === 'move') {
-    const origen = req.body.ruta;
-    const curso = req.body.curso_opcional ? req.body.curso_opcional : req.body.curso;
-    const carpeta = req.body.carpeta_opcional ? req.body.carpeta_opcional : req.body.carpeta;
-    const archivo = `${req.body.archivo}-${req.body.pagina}${req.body.extension}`;
-    const destino = `${req.body.facultad}/${curso}/${carpeta}/${archivo}`;
-    AcademiBot.mueveArchivo(origen, destino)
-      .then(() => {
-        AcademiBot.submissions.eliminaArchivo(req.body.ruta);
-        res.redirect(`/admin/clasificador?tipo=${tipe}&page=${page}`);
-      })
-      .catch(e => {
-        console.log(e);
-        res.render('error', e);
-      });
-  } else if (action === 'delete') {
-    AcademiBot.borraArchivo(req.body.ruta)
-      .then(() => {
-        AcademiBot.submissions.eliminaArchivo(req.body.ruta);
-        res.redirect(`/admin/clasificador?tipo=${tipe}&page=${page}`);
-      })
-      .catch(e => {
-        console.log(e);
-        res.render('error', e);
-      });
-  }
-
+    res.sendStatus(200);
 });
 module.exports = router;
