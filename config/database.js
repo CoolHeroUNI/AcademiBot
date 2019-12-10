9a061239419b701a476e2daaacf1264bc0545bfd
Marco Antonio <mvelar@uni.pe>
2019-12-09 22:31:14 -0500

    DataBase

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+const database = process.env.MYSQL_DATABASE;
+const username = process.env.MYSQL_USERNAME;
+const password = process.env.MYSQL_PASSWORD;
+const timezone = process.env.MYSQL_TIMEZONE;
+const host = process.env.MYSQL_HOST;
+const port = process.env.MYSQL_PORT;
+const dialect = 'mysql';
+
+const sequelize = new Sequelize({ database, host, port, dialect, username, password, timezone });
+
+export default  sequelize;
