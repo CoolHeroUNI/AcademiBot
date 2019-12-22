870a96f969645f005f09607887ea03a612d47d88
Marco Antonio <mvelar@uni.pe>
2019-12-21 21:36:17 -0500

    Test A database test was implemented to ensure the model works.

--- test/dbTest.js
+++ test/dbTest.js
@@ -3,7 +3,12 @@ const entidades = require("../schema/Entidades");
 const relaciones = require("../schema/Relaciones");
 const crearTodo = require("../schema/transacciones/crearTodo");
 const canalMensajeria = require("../schema/Entidades/CanalMensajeria");
-
+const actualizarMensaje = require("../schema/transacciones/actualizarMensaje");
+const actualizarDonacion = require("../schema/transacciones/actualizarDonacion");
+const actualizarEnvio = require("../schema/transacciones/actualizarEnvio");
+const getIdCuentaDonador = require("../schema/transacciones/operaciones/getIdCuentaDonador");
+const getIdCuentaMensajeria = require("../schema/transacciones/operaciones/getIdCuentaMensajeria");
+const getIdCuentaSolicitante = require("../schema/transacciones/operaciones/getIdCuentaSolicitante");
 for (const entidad of entidades) {
   entidad.sync();
 }
@@ -13,7 +18,20 @@ for (const cuenta of cuentas) {
 for (const relacion of relaciones) {
   relacion.sync();
 }
-const nombre = "WHATSAPP";
-canalMensajeria.create({ nombre })
-  .then(() => crearTodo(nombre, "909984655"))
-  .then(() => crearTodo(nombre, "646464665"));
+
+const canal = "WHATSAPP";
+const codigo = "9091354414";
+const recurso = "Lineal/1pc/21-4.jpg";
+async function test() {
+  await canalMensajeria.findOrCreate({ where: { nombre: canal }, defaults: { nombre: canal }});
+  const id = await crearTodo(canal, codigo);
+  const cuentaDonador = await getIdCuentaDonador(id);
+  await actualizarDonacion(cuentaDonador, recurso);
+  const cuentaMensaje = await getIdCuentaMensajeria(id);
+  await actualizarMensaje(cuentaMensaje);
+  const cuentaEnvio = await getIdCuentaSolicitante(id);
+  await actualizarEnvio(cuentaEnvio, true, recurso);
+}
+test()
+  .then(() => console.log('Finished!'))
+  .catch(e => console.error(e));
