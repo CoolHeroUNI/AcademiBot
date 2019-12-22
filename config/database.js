1f25a630ae3375c9517128f2aec7709142700c5e
Marco Antonio <mvelar@uni.pe>
2019-12-22 12:25:26 -0500

    Pooling connection options

--- config/database.js
+++ config/database.js
@@ -7,7 +7,13 @@ const timezone = process.env.MYSQL_TIMEZONE;
 const host = process.env.MYSQL_HOST;
 const port = process.env.MYSQL_PORT;
 const dialect = 'mysql';
-
-const sequelize = new Sequelize({ database, host, port, dialect, username, password, timezone });
+const pool = {
+  acquire: 30000,
+  max: 15,
+  min: 1,
+  idle: 10000,
+  evict: 10000
+};
+const sequelize = new Sequelize({ database, host, port, dialect, username, password, timezone, pool });
 
 module.exports = sequelize;
