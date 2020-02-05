bcab3cbd04b0ec22fe65ccfb1596dc0cd6b4dd7d
Marco Vela <mvelar@uni.pe>
2020-02-04 21:08:52 -0500

    Hotfix

--- controllers/facebookController.js
+++ controllers/facebookController.js
@@ -201,7 +201,7 @@ async function executeCommand(user, command, parameters) {
       await nuevoUsuario(user);
       return regularizaUsuario(user);
     case 'ResetEspecialidad':
-      await E.actualizarInfoUsuario(user, { especialidad_id: null });
+      await E.actualizarInfoUsuario(user, { especialidad_id: null, ruta: '' });
       return regularizaUsuario(user);
     case 'ResetCiclo':
       await E.actualizarInfoUsuario(user, { ciclo_id: null });
